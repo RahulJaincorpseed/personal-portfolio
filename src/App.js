21933c1803e17a07919a904aa16ae1c8ffb7da76
RahulJaincorpseed <rahul.jain@corpseed.com>
2023-07-27 11:52:25 +0530

    add navlinks with css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Footer from './component/Footer';
+import Navbar from './component/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Portfolio</h1>
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<div>Hero Section</div>} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
