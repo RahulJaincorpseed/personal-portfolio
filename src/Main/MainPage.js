b4dbce68588f617bbfa280bc01eb7418ec679263
RahulJaincorpseed <rahul.jain@corpseed.com>
2023-07-28 10:10:55 +0530

    add some css code

--- src/Main/MainPage.js
+++ src/Main/MainPage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import HeroSection from "./HeroSection";
 import MySkills from "./MySkills";
-import Gallery from "../component/Gallery";
 
 const MainPage = () => {
   return (
--- src/Main/MySkills.js
+++ src/Main/MySkills.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React from "react"
 import "./MySkills.scss"
 
 const MySkills = () => {
   return (
     <div className="skill-section section-padding">
-        <div>
-            
+      <div>
+        <h3 className="main-heading">My Skills</h3>
+        <div className="skills-item">
+          <div className="ball-item">
+          
+            <i class="fa-brands fa-github"></i>
+            <p>Github</p>
+          </div>
+          <div className="ball-item">item1</div>
+          <div className="ball-item">item1</div>
+          <div className="ball-item">item1</div>
+          <div className="ball-item">item1</div>
         </div>
+      </div>
     </div>
   )
-};
+}
 
-export default MySkills;
+export default MySkills
