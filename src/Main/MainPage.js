578f63a0e524a8c8b6466322df288413a5bfd516
RahulJaincorpseed <rahul.jain@corpseed.com>
2023-07-28 17:34:49 +0530

    add experience section ui

--- src/Main/MainPage.js
+++ src/Main/MainPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "./HeroSection";
 import MySkills from "./MySkills";
+import WorkExperience from "./WorkExperience";
 
 const MainPage = () => {
   return (
     <div>
         <HeroSection />
         <MySkills />
+        <WorkExperience />
     </div>
   )
 };
--- src/Main/WorkExperience.js
+++ src/Main/WorkExperience.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React from "react"
+import "./WorkExperience.scss"
+import ExperienceCard from "../component/ExperienceCard"
 
 const WorkExperience = () => {
-  return <div>WorkExperience</div>;
-};
+  const leftCard = 1
+  return (
+    <div className="work-experience section-padding">
+      <h3 className="main-heading">My Experience</h3>
+      <div className="experience-section">
+        <div className="letter-height">
+          <div className="circle-experience">
+            <div className="connecting-line"></div>
+          </div>
+          <ExperienceCard />
+        </div>
+        <div className="letter-height">
+          <div className="circle-experience">
+            <div className="connecting-line"></div>
+          </div>
+          <ExperienceCard leftCard={leftCard} />
+        </div>
+        <div className="letter-height">
+          <div className="circle-experience"></div>
+          <ExperienceCard />
+        </div>
+      </div>
+    </div>
+  )
+}
 
-export default WorkExperience;
+export default WorkExperience
