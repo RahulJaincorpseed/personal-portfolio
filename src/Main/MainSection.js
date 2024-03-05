9316c95970c9c31d64c847fdb8f174968e122c26
RahulJaincorpseed <rahul.jain@corpseed.com>
2024-03-05 09:39:49 +0530

    minor ui changes

---
+++ src/Main/MainSection.js
@@ -0,0 +1,20 @@
+import React from "react"
+import HeroSection from "./HeroSection"
+import MySkills from "./MySkills"
+import WorkExperience from "./WorkExperience"
+import Certification from "./Certification"
+import ContactUs from "./ContactUs"
+
+const MainSection = () => {
+  return (
+    <>
+      <HeroSection />
+      <MySkills />
+      <WorkExperience />
+      <Certification />
+      <ContactUs />
+    </>
+  )
+}
+
+export default MainSection
