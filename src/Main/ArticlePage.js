6a16960ea7e7eed2660980223caed9f30837c344
RahulJaincorpseed <rahul.jain@corpseed.com>
2024-03-05 14:26:46 +0530

    new article page and some blank pages added and also add some route

--- src/Main/ArticlePage.js
+++ src/Main/ArticlePage.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React from "react"
+import ArticleBigCard from "../component/ArticleBigCard"
+import { ArticleData } from "../data/ArticleData"
 
 const ArticlePage = () => {
-  return <div>ArticlePage</div>;
-};
+  return (
+    <div className="article-layout">
+      <h2 className="main-heading">Articles</h2>
+      {ArticleData?.map((article, key) => (
+        <ArticleBigCard key={key} article={article} />
+      ))}
+    </div>
+  )
+}
 
-export default ArticlePage;
+export default ArticlePage
